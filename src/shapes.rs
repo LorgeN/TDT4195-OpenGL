969d77f765caeada971d8aac99c53643bf7a1cf0
Eirik Lorgen Tanberg <eirik@tanberg.org>
2021-09-02 23:32:41 +0200

    Remove unnecessary "vec!"

--- src/shapes.rs
+++ src/shapes.rs
@@ -13,7 +13,7 @@ pub fn generate_triangles(x: u32, y: u32) -> (Vec<f32>, Vec<u32>) {
             let x_curr = -1.0 + x_off as f32 * x_dist;
             let y_curr = -1.0 + y_off as f32 * y_dist;
 
-            vert.extend(vec![
+            vert.extend([
                 x_curr,
                 y_curr + y_size,
                 0.0,
@@ -36,9 +36,9 @@ pub fn generate_circle(r: f32, segments: u32) -> (Vec<f32>, Vec<u32>) {
 
     for seg in 0..segments {
         let angle = 2.0 * std::f32::consts::PI * seg as f32 / segments as f32;
-        vert.extend(vec![r * angle.sin(), r * angle.cos(), 0.0]);
+        vert.extend([r * angle.sin(), r * angle.cos(), 0.0]);
         // Have to add segments to seg for last index because we are using unsigned ints
-        indices.extend(vec![0, seg + 1, (seg + segments - 1) % segments + 1]);
+        indices.extend([0, seg + 1, (seg + segments - 1) % segments + 1]);
     }
 
     (vert, indices)
@@ -59,7 +59,7 @@ pub fn generate_spiral(r: f32, segments: u32, circles: u32, end_width: f32) -> (
     for seg in 0..total_seg {
         let angle = 2.0 * std::f32::consts::PI * (seg % segments) as f32 / segments as f32;
         
-        vert.extend(vec![
+        vert.extend([
             // Inner point
             (curr_r - width) * angle.sin(),
             (curr_r - width) * angle.cos(),
@@ -76,10 +76,10 @@ pub fn generate_spiral(r: f32, segments: u32, circles: u32, end_width: f32) -> (
         let index = seg * 2;
 
         if seg > 1 {
-            indices.extend(vec![index, index + 1, index - 2]);
+            indices.extend([index, index + 1, index - 2]);
         }
 
-        indices.extend(vec![index + 3, index + 1, index]);
+        indices.extend([index + 3, index + 1, index]);
     }
 
     (vert, indices)
